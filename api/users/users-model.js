b75443b7a0087a28c181874c6409c4e20a8e9a44
Austin Greer <agreer.ag12@gmail.com>
2021-05-20 11:15:28 -0400

    all model functions are built

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -21,9 +21,14 @@ function findBy(filter) {
       }
     ]
    */
+
+  return db('users')
+    .join('roles', 'users.role_id', '=', 'roles.role_id')
+    .select('user_id', 'username', 'password', 'role_name')
+    .where(filter)
 }
 
-function findById(user_id) {
+async function findById(user_id) {
   /**
     You will need to join two tables.
     Resolves to the user with the given user_id.
@@ -34,6 +39,14 @@ function findById(user_id) {
       "role_name": "instructor"
     }
    */
+
+    const user = await db('users')
+      .join('roles', 'users.role_id', '=', 'roles.role_id')
+      .select('user_id', 'username', 'role_name')
+      .where('user_id', user_id)
+      .first()
+  
+    return user
 }
 
 /**
