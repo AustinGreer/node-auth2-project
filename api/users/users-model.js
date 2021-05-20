0566adcbb7235a2dfd88774c7f0db229ae5ce95d
Austin Greer <agreer.ag12@gmail.com>
2021-05-20 10:40:27 -0400

    find model function working as expected. users endpoint working as expected except it is not restricted

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,23 +1,10 @@
 const db = require('../../data/db-config.js');
 
+// returns an array of objects containing info about a user and their role
 function find() {
-  /**
-    You will need to join two tables.
-    Resolves to an ARRAY with all users.
-
-    [
-      {
-        "user_id": 1,
-        "username": "bob",
-        "role_name": "admin"
-      },
-      {
-        "user_id": 2,
-        "username": "sue",
-        "role_name": "instructor"
-      }
-    ]
-   */
+  return db('users')
+    .join('roles', 'users.role_id', '=', 'roles.role_id')
+    .select('user_id', 'username', 'role_name')
 }
 
 function findBy(filter) {
